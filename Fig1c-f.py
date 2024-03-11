5e4ecc446d69983ac75d8c2b01b281d096ccaab6
Vit Piskovsky <88036393+vit-pi@users.noreply.github.com>
2024-03-11 08:17:10 +0000

    Update Fig1c-f.py

--- Fig1c-f.py
+++ Fig1c-f.py
@@ -3,8 +3,10 @@ import ODE_solve as od
 
 # SIMULATION
 simulation = od.Simulation()
-#simulation.plot_invasion_dynamics("Fig1c")
 simulation.plot_invasion(False,True)
+simulation = od.Simulation()
 simulation.plot_invasion(True,True)
+simulation = od.Simulation()
 simulation.plot_invasion(False,False)
+simulation = od.Simulation()
 simulation.plot_invasion(True,False)
