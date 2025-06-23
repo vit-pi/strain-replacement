ac199a8b8fb4fbb8ee0461f9af1bf491bec6d5ac
Vit Piskovsky <88036393+vit-pi@users.noreply.github.com>
2025-06-23 10:57:04 +0100

    Delete Fig1b.py

--- Fig1b.py
+++
@@ -1,9 +0,0 @@
-# IMPORTS
-import ODE_solve as od
-
-# MAKE PLOT
-simulation = od.Simulation()
-simulation.plot_phase_diagram(1,False,False, False)
-simulation.plot_phase_diagram(1,True,False, False)
-simulation.plot_phase_diagram(1,False,True, False)
-simulation.plot_phase_diagram(1,True,True, False)
